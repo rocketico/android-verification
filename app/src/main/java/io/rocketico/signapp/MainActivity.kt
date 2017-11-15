ef8f0b4ce1e91f9bb7e6d2bef1b73698ef9f9d2e
Uxname <uxname@gmail.com>
2017-11-16 01:35:26 +0300

    Improved style. Added Login Fragment.

--- app/src/main/java/io/rocketico/signapp/MainActivity.kt
+++ app/src/main/java/io/rocketico/signapp/MainActivity.kt
@@ -1,16 +1,12 @@
 package io.rocketico.signapp
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mikepenz.materialdrawer.DrawerBuilder
-import com.nbsp.materialfilepicker.ui.FilePickerActivity
-import devliving.online.securedpreferencestore.SecuredPreferenceStore
-import io.rocketico.signapp.fragment.MainFragment
+import io.rocketico.signapp.fragment.LoginFragment
 import io.rocketico.signapp.fragment.OnFragmentInteractionListener
-import io.rocketico.signapp.utils.Cc
 
 
 class MainActivity : AppCompatActivity(), OnFragmentInteractionListener {
@@ -21,7 +17,7 @@ class MainActivity : AppCompatActivity(), OnFragmentInteractionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.container,MainFragment.newInstance()).commit();
+        supportFragmentManager.beginTransaction().replace(R.id.container, LoginFragment.newInstance()).commit();
         DrawerBuilder().withActivity(this).build()
     }
 
---
+++ app/src/main/java/io/rocketico/signapp/fragment/LoginFragment.kt
@@ -0,0 +1,34 @@
+package io.rocketico.signapp.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.rocketico.signapp.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_login, container, false)
+    }
+
+    companion object {
+
+        fun newInstance(): LoginFragment {
+            val fragment = LoginFragment()
+            val args = Bundle()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+}
